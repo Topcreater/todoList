07edad4d3a8859cd5b75867177873824fea62f00
maramzan <arsalanramzan811@gmail.com>
2023-11-14 13:29:54 +0500

    first commit

---
+++ src/screens/ CurrentTodos/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const CurrentTodos = () => {
+    return (
+        <View>
+            <Text>Welcome to the Home Screen!</Text>
+        </View>
+    );
+};
+
+export default CurrentTodos;
